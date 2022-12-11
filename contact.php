c469f61e6c95c21c6d2e6b17fab16092e758579d
Reece741 <47571092+Reece741@users.noreply.github.com>
2022-12-11 14:01:59 +0000

    First Commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailForm = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "reecematthewtaylor@gmail.com";
+    $headers = "From: ".$mailFrom; 
+    $txt = "You have a message ".$name".\n\n".$message;
+
+    mail($mailTo, $name, $txt ,$headers);
+
+    header("Location: index.html?Message Sent")
+}
+
+
+?>
